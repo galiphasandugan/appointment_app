510e4de6ca31e7e9559792e77d67d02cc457a898
galiphasandugan <galiphasandugan@gmail.com>
2023-07-25 23:55:43 +0200

    hotfix:date format

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -1,26 +1,39 @@
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
 
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-
-function AddModal({show, handleClose}) {
- 
+function AddModal({ show, handleClose }) {
+    const [name,setName]=useState("")
+    const [date,setDate]=useState(new Date().toISOString().slice(0,10))
+    console.log(name,date)
   return (
     <>
-      
-
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Appointment for...</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Patient Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter name" onChange={(e)=> setName(e.target.value)} value={name} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Date</Form.Label>
+              <Form.Control type="date" placeholder="date" onChange={(e)=> setDate(e.target.value)} value={date} />
+            </Form.Group>
+            <div className="text-center">
+              <Button variant="success" type="submit"className="me-2" >
+                Save
+              </Button>
+              <Button variant="danger" onClick={handleClose}>
+                Close
+              </Button>
+            </div>
+          </Form>
+        </Modal.Body>
       </Modal>
     </>
   );
--- src/components/Doctor.jsx
+++ src/components/Doctor.jsx
@@ -9,19 +9,15 @@ import { useState } from "react"
 const Doctor = () => {
     const [show, setShow] = useState(false);
 
-    const handleImgClick = () =>{
-        setShow(true)
-    }
-
   return (
     <div>
       <Container className="p-2">
         <h3 className="display-6 mb-3"style={{color: "rgb(188, 18, 189"}}>Our Doctors</h3>
-        <Row xs={6} sm={4} md={3}>
+        <Row className="justify-content-center">
 
         {doctorData.map(({id,name,dep,img})=>(
-        <Col>
-            <img onClick={handleImgClick} src={img} alt="" className="img-thumbnail doctor-img" />
+        <Col xs={6} sm={4} md={3} key={id}>
+            <img onClick={() => setShow(true)} src={img} alt="" className="img-thumbnail doctor-img" />
             <h5>{name}</h5>
             <h6>{dep}</h6>
         </Col>
